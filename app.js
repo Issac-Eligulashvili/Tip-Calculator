3424e5e3799ffebc6c8323628a0614943c23e948
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-01-25 13:29:31 -0500

    FINSIHED!!

--- app.js
+++ app.js
@@ -13,7 +13,28 @@ function calculateAndDisplay(){
     //Display the Results in the HTML
     document.querySelector('#tipResult').innerText = 'Tip: $' + tip.toFixed(2);
     document.querySelector('#totalBillResult').innerText = 'Total: $' + totalBill.toFixed(2);
-    document.querySelector('#amtPerPerson').innerText = 'Total Per Person: $' + amtPerPerson.toFixed(2);
+    document.querySelector('#amtPerPersonResult').innerText = 'Total Per Person: $' + amtPerPerson.toFixed(2);
 
+    //
+}
 
+// helper function for calculations
+function calcTip(bill, quality) {
+
+    if (quality === 'Great') {
+        return bill * 0.2;
+    } else if (quality === 'Good') {
+        return bill * 0.15;
+    } else {
+        return bill * 0.1;
+    }
+
+}
+
+function calcTotalBill(bill, tip) {
+    return bill + tip
+}
+
+function calcAmtPerPerson (totalBill, numOfPpl) {
+    return totalBill / numOfPpl
 }
